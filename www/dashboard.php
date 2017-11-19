afa127538f6e79b292d30cb911fcaba0e7dfc10e
ritalanoie <ritalanoie@gmail.com>
2017-11-19 02:40:30 -0500

    Commit message1111

--- www/dashboard.php
+++ www/dashboard.php
@@ -6,11 +6,10 @@
 -->
 <!DOCTYPE html>
  	<?php
-		sec_session_start();
-		include_once '../includes/session.php'; //start the session
-		include_once '../includes/database_connect.php'; //start the session
+		//include_once '../includes/session.php'; //start the session
+		//include_once '../includes/database_connect.php'; //start the session
 		include_once '../includes/function.php';
-
+		
 print ('user is');
   $username = $_SESSION['user'];
 		print ($_SESSION['username']);
